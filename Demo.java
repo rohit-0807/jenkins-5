f39fa9aa5fff02b86f2ce2be2d9fa5c3024d3db9
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-05-14 09:52:23 +0530

    save

---
+++ Demo.java
@@ -0,0 +1,11 @@
+
+
+public class Demo {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        int divide = a / b;
+        System.out.println("Division of " + a + " and " + b + " is: " + divide);
+    }
+}
